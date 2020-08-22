027ec78f17f175113d16a1df093cee76dc43c2d8
Kate Hile <kathrynehile@gmail.com>
2020-08-22 17:19:24 -0500

    again

--- script.js
+++ script.js
@@ -8,10 +8,7 @@ function writePassword() {
 
   passwordText.value = password;
 
-
 }
-
-
 
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
@@ -19,18 +16,17 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', "'", '=', '<', '>', '/', ',', '.', '`'];
 
 
-
 function generatePassword() {
 
 
-  var passwordLength = prompt('Your password can be between 8 - 128 characters. How long would you like your password to be? (whole numbers only)');
+  var passwordLength = prompt("Your password can be between 8 - 128 characters. How long would you like your password to be? (whole numbers only)");
 
 
   if (!passwordLength) {
-    alert('No value entered, try again.');
+    alert("No value entered, try again.");
     return;
   } else if (passwordLength < 8 || passwordLength > 128) {
-    alert('Please enter a password with ONLY 8 - 128 characters.');
+    alert("Please enter a password with ONLY 8 - 128 characters.");
     return;
   } else {
     var confirmLowerCase = confirm("Any lowercase letters in your password?");
@@ -40,7 +36,7 @@ function generatePassword() {
 
 
     if (!confirmLowerCase && !confirmUpperCase && !confirmNumbers && !confirmSpecial) {
-      alert('You must select at least one, try again');
+      alert("You must select at least one, try again");
     } else {
       var allChar = [];
       if (confirmLowerCase) allChar = allChar.concat(lowerCase);
